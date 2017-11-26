ea80198914ef267dd5b9e721b44a9d7c203c6006
chao.lu <lc_yizhiyi@163.com>
2017-11-26 16:38:11 +0800

    变量与环境配置

---
+++ config_environment.py
@@ -0,0 +1,52 @@
+stanford_corenlp_root_path = r"C:/stanford-corenlp-full-2017-06-09/stanford-corenlp-full-2017-06-09/"
+
+# copus__dimension__vocabulary size__author__architecture__training algorithm__context window
+# ----------------------------------  Word2Vec  ------------------------------------------------------
+# Google News___300d___3M___Google___word2vec___negative sampling___BoW - ~5
+word_vec_file1 = "C:/Users/Administrator/Desktop/wordwec/deps.words/deps.words"
+
+# -----------------------------------  GloVe  --------------------------------------------------------
+# Twitter___50d___1.2M___Glove___GloVe___GloVe___AdaGrad
+word_vec_file2 = "C:/Users/Administrator/Desktop/wordwec/glove.6B/glove.6B.50d.txt"
+# Twitter___100d___1.2M___Glove___GloVe___GloVe___AdaGrad
+word_vec_file3 = "C:/Users/Administrator/Desktop/wordwec/glove.6B/glove.6B.100d.txt"
+# Twitter___200d___1.2M___Glove___GloVe___GloVe___AdaGrad
+word_vec_file4 = "C:/Users/Administrator/Desktop/wordwec/glove.6B/glove.6B.200d.txt"
+
+# ------------------------------------  GloVe  -------------------------------------------------------
+# Wikipedia+Gigaword5___50d___400,000___Glove___GloVe___AdaGrad___10+10
+word_vec_file5 = "C:/Users/Administrator/Desktop/wordwec/glove.twitter.27B/glove.twitter.27B.50d.txt"
+# Wikipedia+Gigaword5___100d___400,000___Glove___GloVe___AdaGrad___10+10
+word_vec_file6 = "C:/Users/Administrator/Desktop/wordwec/glove.twitter.27B/glove.twitter.27B.100d.txt"
+# Wikipedia+Gigaword5___200d___400,000___Glove___GloVe___AdaGrad___10+10
+word_vec_file7 = "C:/Users/Administrator/Desktop/wordwec/glove.twitter.27B/glove.twitter.27B.200d.txt"
+
+# 存成字典，方便索引
+word_vec_file = \
+    [word_vec_file1, word_vec_file2, word_vec_file3, word_vec_file4, word_vec_file5, word_vec_file6, word_vec_file7]
+# 生成数据时候的文件名
+word_vec_file_state = ["deps", "glove.6B.50d", "glove.6B.100d", "glove.6B.200d",
+                       "glove.twitter.27B.50d", "glove.twitter.27B.100d", "glove.twitter.27B.200d"]
+
+
+
+cat_names = \
+    ['Cause-Effect(e1,e2)', 'Component-Whole(e1,e2)', 'Content-Container(e1,e2)', 'Entity-Destination(e1,e2)',
+     'Entity-Origin(e1,e2)', 'Instrument-Agency(e1,e2)', 'Member-Collection(e1,e2)', 'Message-Topic(e1,e2)',
+     'Product-Producer(e1,e2)', 'Other', 'Cause-Effect(e2,e1)', 'Component-Whole(e2,e1)', 'Content-Container(e2,e1)',
+     'Entity-Destination(e2,e1)', 'Entity-Origin(e2,e1)', 'Instrument-Agency(e2,e1)', 'Member-Collection(e2,e1)',
+     'Message-Topic(e2,e1)', 'Product-Producer(e2,e1)']
+
+
+def create_cat_map(cat_names):
+    cat_map = {}
+    label = 0
+    for cat in cat_names:
+        cat_map[cat_names[label]] = label + 1
+        label += 1
+    return cat_map
+
+
+
+
+
